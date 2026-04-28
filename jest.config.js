f5cc20bec9172bd31c40d469c8649d0afccf7457
Juani Viola <juaniviola1297@gmail.com>
2026-04-27 23:12:46 -0300

    chore: Update Jest configuration and dependencies in package.json and package-lock.json

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
-const path = require('path');
-
 module.exports = {
   rootDir: '.',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
-    '^.+\\.ts$': [require.resolve('ts-jest', { paths: [path.resolve(__dirname, '../../backend/node_modules')] }), { tsconfig: '<rootDir>/tsconfig.json' }],
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
   },
   moduleNameMapper: {
     '^@lib/(.*)$': '<rootDir>/src/$1',
